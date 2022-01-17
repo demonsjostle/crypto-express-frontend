5937d6e860ed693a84ea69f18e36d45d75506802
KIATISAK SOMKHUNNGUEN <6010110044@psu.ac.th>
2022-01-17 10:54:12 +0700

    project update 17-1-22

--- crypto-express-frontend/src/Pages/WhitePaper/index.js
+++ crypto-express-frontend/src/Pages/WhitePaper/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Navbar} from "../../Config";
 
 const index = () => {
     return (
+
+        <>
+        <Navbar />
         <div>
             Whitepaper
         </div>
+        </>
     )
 }
 
